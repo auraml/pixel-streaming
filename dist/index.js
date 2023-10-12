265d598301c0147fb8da226ea2a816840820b70c
CreatoonAuraML <raj.aryan@auraml.com>
2023-10-13 00:06:16 +0530

    making this package compatible for VITE

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,3 @@
+"use strict"
 
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./pixel-streaming.cjs.production.min.js')
-} else {
-  module.exports = require('./pixel-streaming.cjs.development.js')
-}
+module.exports = require("./pixel-streaming.esm.js")
